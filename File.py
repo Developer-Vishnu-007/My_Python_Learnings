4e5dd8e6caff077c9336e882d2cc7eb54fb805b1
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-08-24 21:39:03 +0530

    Update File.py

--- File.py
+++ File.py
@@ -1,6 +1,6 @@
 #My Learnings in python
-print("Hello World")
-v = "It is a variable"
+print("Hello World") #print displays the output in screen
+v = "It is a variable" #assigning a string to a variable
 
 
 
