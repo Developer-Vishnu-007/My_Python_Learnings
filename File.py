de51d67a0c712e5ebbff83b6391af3ca1a390f0c
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-08-29 08:17:41 +0530

    Update File.py

--- File.py
+++ File.py
@@ -1,3 +1,5 @@
+import #imports a module or a library 
+
 #My Learnings in python
 print("Hello World") #print displays the output in screen
 v = "It is a variable" #assigning a string to a variable
