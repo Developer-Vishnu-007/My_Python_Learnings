2c1b04b39ad37ed6ce3fe61215c449619eedd6aa
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-09-02 22:57:42 +0530

    Update File.py

--- File.py
+++ File.py
@@ -15,6 +15,7 @@ import json
 #My Learnings in python
 
 print("Hello World") #print displays the output in screen
+
 v = "It is a variable" #assigning a string to a variable
 int = 10 #it is a number (integer)
 float = 10.0 #num float
