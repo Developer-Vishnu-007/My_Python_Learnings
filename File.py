c047d9da8d2b3aacc861c80916337deb505d79ab
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-09-16 18:31:41 +0530

    Update File.py

--- File.py
+++ File.py
@@ -27,7 +27,7 @@ b = [1,2,3,4,5] #defining a list
 print(b) #prints the list
 
 for i in b: #loop
-  print(i) #i is iteration
+  print(i) #i is iteration variable
   
 a = int(input("Enter a number: ")) #geting a integer as a input
 
