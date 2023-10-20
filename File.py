c3d31ebbe5490895bbc5a81134f2ff9dfb34b74e
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-10-20 11:49:52 +0530

    Update File.py

--- File.py
+++ File.py
@@ -18,7 +18,7 @@ print("Hello World")#print displays the output in screen
 
 v = "It is a variable"#assigning a string to a variable
 
-int = 10 #it is a number (integer)
+int = 10#it is a number (integer)
 
 float = 10.0 #num float
 
