1224ccc5b4f5e1b9364a18d623b93e699703c868
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-10-20 11:47:59 +0530

    Update File.py

--- File.py
+++ File.py
@@ -16,7 +16,7 @@ import#json
 
 print("Hello World") #print displays the output in screen
 
-v = "It is a variable" #assigning a string to a variable
+v = "It is a variable"#assigning a string to a variable
 
 int = 10 #it is a number (integer)
 
