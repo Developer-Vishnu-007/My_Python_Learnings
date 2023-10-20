d9b83900b2dc7234d2a507718c7e8bc3c6df541d
VISHNUVARTHAN S <91307356+Developer-Vishnu-007@users.noreply.github.com>
2023-10-20 11:48:16 +0530

    Update File.py

--- File.py
+++ File.py
@@ -14,7 +14,7 @@ import#json
 
 #My Learnings in python
 
-print("Hello World") #print displays the output in screen
+print("Hello World")#print displays the output in screen
 
 v = "It is a variable"#assigning a string to a variable
 
